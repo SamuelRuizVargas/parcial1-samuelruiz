f4300ab17a10686cf2f9143d7d96142ebe537551
SamuelRuizVargas <Samuel.ruizv@udea.edu.co>
2020-10-17 16:02:32 -0500

    Empezado el proceso de codificacion, aun definiendo variables y dandoles valores fisicos segun las entradas

--- main.cpp
+++ main.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    //Variables para el DD
+    int XoD, VxD, YoD, VyD, VoD, alpha, rD, diaD, periD, AcirD;
+    //Variables para el DO
+    int XoO, VxO, YoO, VyO, VoO, beta, rO, diaO, periO, AcirO;
+    //Variables generales
+    float g, T;
+    //Definicion de magnitudes fisicas
+
+    //Definicion de los centros
+    int centroD[2]={XD, YD}, centroO[2]={XO, YO};
+
     return 0;
 }
