cd61aafe37e0b8e3c2917512b09d6fffdd2576d4
SamuelRuizVargas <Samuel.ruizv@udea.edu.co>
2020-10-20 10:58:00 -0500

    Segundo oportunidad, avances en la reformulacion del problema

--- main.cpp
+++ main.cpp
@@ -3,78 +3,50 @@
 
 using namespace std;
 
+float punt_dist(int, int, int, int);
+
 int main()
 {
-    //Variables para el DD
-    int XoD, VxD, YoD, VyD, VoD, alpha, XD,YD;
-    //Variables para el DO
-    int XoO, VxO, YoO, VyO, VoO, beta,  XO,YO;
-    //Variables generales
-    float g=9.8, T=0, rD, diaD, periD, AcirD, rO, diaO, periO, AcirO;
+    int d=600;
+    float g=9.8, pi=3.14159;
+    //para la ofensiva
+    int hO=100, xO=0, yO=hO;
+    float destO=0.05*d;
+    //para la defensiva
+    int hD=100, xD=d, yD= hD;
+    float destD=0.025*d;
 
-    //Pidiendo datos
-    cout<<"A continuacion ingrese las variables del canhon O."<<endl;
-    cout<<"Ingrese la posicion inicial en X del canhon O: ";cin>>XoO;
-    cout<<"Ingrese la posicion inicial en Y del canhon O: ";cin>>YoO;
-    cout<<"Ingrese la velocidad inicial canhon O: ";cin>>VoO;
-    cout<<"Ingrese el angulo desde el cual el canhon O disparo: ";cin>>beta;
+    float anguO=50*pi/180, anguD=0;
 
-    cout<<"A continuacion ingrese las variables del canhon D."<<endl;
-    cout<<"Ingrese la posicion inicial en X del canhon D (tiene que ser mayor al del canhon O): ";cin>>XoD;
-    while(XoD<XoO)
+    bool golpea=false;
+    for (int VoO=0; VoO<=100;VoO+=1)
     {
-        cout<<"Ingrese la posicion inicial en X del canhon D (tiene que ser menor al del canhon O): ";cin>>XoD;
-    }
-    cout<<"Ingrese la posicion inicial en Y del canhon D (tiene que ser mayor al del canhon O): ";cin>>YoD;
-    while(YoD<YoO)
-    {
-        cout<<"Ingrese la posicion inicial en Y del canhon D (tiene que ser mayor al del canhon O): ";cin>>YoD;
-    }
-    cout<<"Ingrese la velocidad inicial canhon D: ";cin>>VoD;
-
-    //Definicion de magnitudes fisicas
-    VxO=VoO*cos(beta);
-    VyO=VoO*sin(beta)-g*T;
-
-    XO=XoO+VxO*T;
-    YO=YoO+VyO*T-1/2*g*(T*T);
-
-    int centroO[2]={XO, YO};
-
-    rO=0.05;
-    diaO=rO*2;
-    periO=2*3.14159*rO;
-    AcirO=3.14159*(rO*rO);
-
-    while(YO>=YoD && XO>=XoD)
-    {
-        T++;
-        XO=XoO+VxO*T;
-        YO=YoO+VyO*T-1/2*g*(T*T);
-    }
-    if(YO==YoD && XO==XoD)
-    {
-
+        int VxO = VoO*cos(anguO);
+        int VyO = VoO*sin(anguO);
+        for (int t=0;t<=1000;t++)
+        {
+            int x=VxO*t;
+            int y=yO + VyO*t-(0.5*g*t*t);
+            if(punt_dist(d, hD, x, y)<=destO)
+            {
+                golpea=true;
+                break;
+            }
+        }
     }
 
-
-
-
-
-    //--------------------------------------------------------------
-    VxD=VoD*cos(alpha);
-    VyD=VoD*sin(alpha)-g*T;
-
-    XD=XoD+VxD*(T-2);
-    YD=YoD+VyD*(T-2)-1/2*g*((T-2)*(T-2));
-
-    int centroD[2]={XD, YD};
-
-    rD=0.025;
-    diaD=rD*2;
-    periD=2*3.14159*rD;
-    AcirD=3.14159*(rD*rD);
 
 
     return 0;
 }
+
+float punt_dist(int x1, int y1, int x2, int y2)
+{
+    int resta1=x2-x1;
+    int resta2=y2-y1;
+    int resta1cuadrada=resta1*resta1;
+    int resta2cuadrada=resta2*resta2;
+    float result=sqrt(resta1cuadrada+resta2cuadrada);
+    return result;
+}
+
